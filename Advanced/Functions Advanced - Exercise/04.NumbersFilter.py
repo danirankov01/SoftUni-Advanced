9aa598f507072f754d3d43acf92abc2958b84f78
danirankov01 <122024239+danirankov01@users.noreply.github.com>
2023-02-16 17:05:38 +0200

    Advanced Functions

--- Advanced/Functions Advanced - Exercise/04.NumbersFilter.py
+++ Advanced/Functions Advanced - Exercise/04.NumbersFilter.py
@@ -5,14 +5,8 @@ def even_odd_filter(**kwargs):
         elif(k == "odd"):
             kwargs[k] = list(filter(lambda x: x % 2 != 0, kwargs[k]))
 
-    kwargs = sorted(kwargs.items(), key=lambda x: (-len(x[1])))
-    return kwargs
-
-
 
 print(even_odd_filter(
     odd=[1, 2, 3, 4, 10, 5],
     even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
 ))
-
-
