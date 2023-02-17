4bf30ead7fcead64eb9a293b303b1c6b8e218776
danirankov01 <122024239+danirankov01@users.noreply.github.com>
2023-02-17 17:39:55 +0200

    FirstStepsInOOP

--- OOP/FirstStepsInOOP - Lab/01.RhombusOfStars.py
+++ OOP/FirstStepsInOOP - Lab/01.RhombusOfStars.py
@@ -1,1 +1,17 @@
-print("Hello")
+def printRombus(n, i):
+    print(' ' * (n - i), end="")
+    for j in range(1, i + 1):
+        print('*', end=" ")
+    print()
+
+
+n = int(input())
+
+for i in range(1, n + 1):
+    printRombus(n, i)
+
+for j in range(n - 1, 0, -1):
+    printRombus(n, j)
+
+
+# print('4' * 4)
