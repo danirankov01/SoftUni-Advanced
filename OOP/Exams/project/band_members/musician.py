577b35bd1f67130844c00cb0b25f48f5db51ef91
danirankov01 <122024239+danirankov01@users.noreply.github.com>
2023-03-17 18:47:45 +0200

    Exams

--- OOP/Exams/project/band_members/musician.py
+++ OOP/Exams/project/band_members/musician.py
@@ -31,10 +31,4 @@ class Musician(ABC):
 
     @abstractmethod
     def learn_new_skill(self, new_skill):
-        if new_skill not in self.skills:
-            raise ValueError(f"{new_skill} is not a needed skill!")
-
-        if new_skill in self.skills:
-            raise Exception(f"{new_skill} is already learned!")
-
-        return f"{self.name} learned to {new_skill}."
+        pass
